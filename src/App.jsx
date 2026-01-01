0eb842c237b69811a436d15afcec6b41c4c763d5
Guilherme <gfernandoportelaoliveira@gmail.com>
2026-01-01 12:46:25 -0300

    Adicionando Audio no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,59 @@
-
+import { useState, useRef } from 'react';
 import './App.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import brasCubasImg from './assets/bras_cubas.jpeg';
 import Capa  from './capa';
+import SeletorCapitulos from './SeletorCapitulos';
+import BotoesControle from './BotoesControle';
+import livro from './assets/capitulos/livro';
+import { use } from 'react';
+import GerenciadorFaixa from './GerenciadorFaixa';
 
 function App() {
+  // O React ele reage a mudanças de variáveis de estado.
+
+  const [taTocando, definirTaTocando] = useState(false)
+  const [faixaAtual, definirFaixaAtual] = useState(0)
+  const tagAudio = useRef(null)
+
+ 
 
   const informacoesLivro = {
     nome:'Memórias Postumas de Brás Cubas',
     autor: 'Machado de Assis',
     totalCapitulos: 2,
     capa: brasCubasImg,
+    capitulos: livro,
     textoAlternativo: 'Capa do livro Memórias Postumas de Brás Cubas.'
   }
+
+  const tocarFaixa = () => {
+    tagAudio.current.play();
+    definirTaTocando(true);
+  };
+
+  const pausarFaixa = () => {
+    tagAudio.current.pause();
+    definirTaTocando(false);
+  };
+
+  const tocarOuPausarFaixa = () => {
+    if(taTocando) {
+      pausarFaixa()
+    } else {
+      tocarFaixa()
+    }
+  };
+
   
   return ( <> 
       <Capa imagemCapa={informacoesLivro.capa} textoAlternativo={informacoesLivro.textoAlternativo}/>
+
+      <SeletorCapitulos capituloAtual={faixaAtual + 1}  />
+      <GerenciadorFaixa faixa={informacoesLivro.capitulos[faixaAtual]} referencia={tagAudio}/>
+      <BotoesControle taTocando={taTocando} tocarOuPausarFaixa={tocarOuPausarFaixa}/>
     </>
+      
   )
 }
 
---
+++ src/BotoesControle.jsx
@@ -0,0 +1,21 @@
+const BotoesControle = (props) => {
+    return <div className="caixa-botoes">
+        <button>
+            <i className="bi bi-skip-start-circle-fill"></i>
+        </button>
+        <button>
+            <i className="bi bi-arrow-counterclockwise"></i>
+        </button>
+        <button onClick={props.tocarOuPausarFaixa}>
+            <i className={`bi bi-${props.taTocando ? "pause" : "play"}-circle-fill`}></i>
+        </button>
+        <button>
+            <i className="bi bi-arrow-clockwise"></i>
+        </button>
+        <button>
+            <i className="bi bi-skip-end-circle-fill"></i>
+        </button>
+    </div>
+}
+
+export default BotoesControle;
